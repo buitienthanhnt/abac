155472b56ccb447d8ced5bd8a6b3d15e76223f48
thanhnt <buitienthanhnt@gmail.com>
2026-07-20 21:46:34 +0700

    dev-navigation: Up code for setup support react-native navigation

--- App.tsx
+++ App.tsx
@@ -5,14 +5,42 @@
  * @format
  */
 
-import { ScrollView, StatusBar, StyleSheet, useColorScheme } from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View} from 'react-native';
 import {
   SafeAreaProvider,
 } from 'react-native-safe-area-context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import SliverChartScreen from './src/screens/SliverChartScreen';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function AppContent() {
+  return (
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <SliverChartScreen />
+    </ScrollView>
+  );
+}
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screens: {
+    Home: HomeScreen,
+    App: AppContent,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -20,20 +48,11 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <AppContent />
+        <Navigation />
+        {/*<StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />*/}
+        {/*<AppContent />*/}
       </SafeAreaProvider>
     </QueryClientProvider>
-  );
-}
-
-function AppContent() {
-
-  return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-      <SliverChartScreen />
-    </ScrollView>
-
   );
 }
 
